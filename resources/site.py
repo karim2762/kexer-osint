dc2853ce0042b1e2bca4948cb3f5acbe1355d657
Kareem Basha <karimullaskmk1234@gmail.com>
2026-03-01 13:18:19 +0530

    Create site.py

---
+++ resources/site.py
@@ -0,0 +1,17 @@
+sites = {
+    "Instagram": "https://www.instagram.com/{username}",
+    "GitHub": "https://github.com/{username}",
+    "TikTok": "https://www.tiktok.com/@{username}",
+    "Reddit": "https://www.reddit.com/user/{username}",
+    "YouTube": "https://www.youtube.com/@{username}",
+    "Pinterest": "https://www.pinterest.com/{username}",
+    "Twitch": "https://www.twitch.tv/{username}",
+    "Medium": "https://medium.com/@{username}",
+    "Facebook": "https://www.facebook.com/{username}",
+    "X": "https://x.com/{username}",
+    "Steam": "https://steamcommunity.com/id/{username}",
+    "SoundCloud": "https://soundcloud.com/{username}",
+    "Vimeo": "https://vimeo.com/{username}",
+    "Behance": "https://www.behance.net/{username}",
+    "DeviantArt": "https://www.deviantart.com/{username}"
+}
